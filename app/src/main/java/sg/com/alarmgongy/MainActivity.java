27a0c2b53bc903b6f11592e3a74ebae70ec46488
Stevan Grubac <nadreal@gmail.com>
2019-10-14 16:28:25 +0200

    Design Refactored

--- app/src/main/java/sg/com/alarmgongy/MainActivity.java
+++ app/src/main/java/sg/com/alarmgongy/MainActivity.java
@@ -2,102 +2,51 @@ package sg.com.alarmgongy;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.CountDownTimer;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity  {
-    private TextView mTextView, tvStbTime;
+    private TextView  tvAlarmTime, tvCurrentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = findViewById(R.id.textView);
-        tvStbTime = findViewById(R.id.tv_stb_time);
+        tvAlarmTime = findViewById(R.id.tv_alarm_time);
+        tvCurrentTime = findViewById(R.id.tv_current_time);
 
-        /*
-        String hours = String.valueOf(c.get(Calendar.HOUR));
-        String minutes = String.valueOf(c.get(Calendar.MINUTE));
-        mTextView.setText(hours + " : " + minutes);
-        Log.d("GRUBI" , "on Create!");
-        Button buttonTimePicker = findViewById(R.id.button_timepicker);
-        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment timePicker = new TimePickerFragment();
-                timePicker.show(getSupportFragmentManager(), "time picker");
-                Log.d("GRUBI" , "open time picker!");
-            }
-        });
-
-
-        Button buttonCancelAlarm = findViewById(R.id.button_cancel);
-        buttonCancelAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cancelAlarm();
-            }
-        });
-        */
         setAlarm();
     }
 
     public void setAlarm() {
-
-        Log.d("GRUBI" , "inside on TIMESET");
         Calendar c = Calendar.getInstance();
         c.set(Calendar.HOUR_OF_DAY, 21);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
 
         updateTimeText(c);
-        startTimer(c);
         startAlarm(c);
+
+//      startTimer(c);
     }
 
-    public void startTimer(Calendar c) {
-        long test = Calendar.getInstance().getTimeInMillis() - c.getTimeInMillis();
-
-
-        new CountDownTimer(test, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                tvStbTime.setText("seconds remaining: " + millisUntilFinished / 1000);
-                //here you can have your logic to set text to edittext
-            }
-
-            public void onFinish() {
-                tvStbTime.setText("done!");
-                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-                r.play();
-            }
-
-        }.start();
-    }
     private void updateTimeText(Calendar c) {
-        String timeText = "Alarm set for: ";
+        String timeText = "Alarm is set for: ";
         timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
-
-
-        mTextView.setText(timeText);
+        tvAlarmTime.setText(timeText);
+        tvCurrentTime.setText((CharSequence) c.getTime());
     }
 
     private void startAlarm(Calendar c) {
@@ -107,21 +56,35 @@ public class MainActivity extends AppCompatActivity  {
 
         if (c.before(Calendar.getInstance())) {
             c.add(Calendar.DATE, 1);
-            Log.d("GRUBI", "Alarm is set before desired time");
         }
-
 
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
     }
 
-    /*
     private void cancelAlarm() {
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, AlertReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
 
         alarmManager.cancel(pendingIntent);
-        mTextView.setText("Alarm canceled");
+        tvAlarmTime.setText("Alarm canceled");
     }
-    */
+
+    public void startTimer(Calendar c) {
+        long test = Calendar.getInstance().getTimeInMillis() - c.getTimeInMillis();
+        new CountDownTimer(test, 1000) {
+            public void onTick(long millisUntilFinished) {
+                tvAlarmTime.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                tvAlarmTime.setText("done!");
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                r.play();
+            }
+        }.start();
+    }
+
+
 }
